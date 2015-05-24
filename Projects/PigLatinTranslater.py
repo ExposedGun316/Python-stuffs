ce0dac2458ef932247ec367b5c4993a73f4897b9
ExposedGun316 <exposedgun316@live.co.uk>
2015-05-24 18:04:36 +0100

    Added in comments to remind me what everything does

--- Projects/PigLatinTranslater.py
+++ Projects/PigLatinTranslater.py
@@ -1,13 +1,14 @@
 original = input('Enter a word:')
 
-if len(original) > 0 and original.isalpha():
+if len(original) > 0 and original.isalpha(): #Making sure user selected word and make sure it is a word and not a number
     print (original)
 else:
-    print ('empty')
-    
+    print ('empty') #If input is a number or nothing inputed, don't continue with code
+
+#begun, the translating has    
 pyg = 'ay'
-word = original.lower()
+word = original.lower() #Changes word to lowercase
 first = word[0]
-new_word = word + first + pyg
-new_word = new_word[1:len(new_word)]
-print (new_word)
+new_word = word + first + pyg #Adds all above elements together
+new_word = new_word[1:len(new_word)]#Slices word
+print (new_word) #Prints the Pig Latin word
